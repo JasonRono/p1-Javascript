06bf7e4324ebe1b92233cc89a24f64fd2fd9bb49
Jason <9020599@student.zadkine.nl>
2022-09-13 22:12:31 +0200

    Opdracht 3.1 af en gepushed naar me repo op Git.

---
+++ data-types/app.js
@@ -0,0 +1,46 @@
+//Oefening 1.
+const Myname = 'Jason';
+let Myage = 20;
+const diploma = true;
+var passed;
+
+document.write(Myname + '<br>');
+document.write(Myage + '<br>');
+document.write(diplomas + '<br>');
+document.write(passed + '<br>');
+
+//Oefening 2.
+let num1 = 15;
+let num2 = 10;
+let answer;
+
+//Hier word de operator keer gebruikt.
+answer = num1*num2;
+document.write(answer + '<br>');
+
+
+//Hier word de operator plus gebruikt.
+let num3 = 5;
+let num4 = 2;
+let answer2 = num3 + num4;
+document.write(answer2 + '<br>');
+
+//Hier word de operator min gebruikt.
+let num5 = 10;
+let num6 = 4;
+let answer3 = num5-num6;
+document.write(answer3 + '<br>');
+
+//Hier word de operator gedeeld door gebruikt.
+let num7 = 60;
+let num8 = 30;
+let answer4 = num7/num8;
+document.write(answer4 + '<br>');
+
+//Hier word de operator rest waarde gebruikt.
+let num9 = 100;
+let num10 = 20;
+let answer5 = num9 % num10;
+document.write(answer5 + '<br>');
+
+
