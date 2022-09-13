c83535533c4d9d15628a071b852e4477dc847717
Jason <9020599@student.zadkine.nl>
2022-09-13 22:34:36 +0200

    Opdracht 3.2 gemaaktt en af.

---
+++ onderzoek/app.js
@@ -0,0 +1,20 @@
+//Oefening 1.
+document.write(
+    'Een comparison operator is om bepaalde waardes een waar of niet waar conditie aan te geven of met elkaar vergelijken of waardes te bepalen.<br> '
+    );
+document.write(
+    'Een coditional statement is om bepaalde waardes een toewijzing te geven doormiddel van een voorwaarde.<br>'
+    );
+
+//Oefening 2.
+let kleinerDan = 2 < 12;
+document.write(kleinerDan + '<br>');//Geeft true aan.
+let groterDan = 10 > 5;
+document.write(groterDan + '<br>');//Geeft true aan.
+let gelijkAan = 20 == 20;
+document.write(gelijkAan + '<br>');//Geeft true aan.
+
+//Oefening 3.
+let num1 = 15;
+let num2 = 5;
+document.write(num1 == 5 || num2 == 15);//Geeft false aan.
