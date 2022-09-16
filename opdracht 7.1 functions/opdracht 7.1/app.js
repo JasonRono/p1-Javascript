22df3401aae952183ad323365f6a98b22f304a32
Jason <9020599@student.zadkine.nl>
2022-09-16 14:56:13 +0200

    Oefening 1 gemaakt en getest en af.

---
+++ opdracht 7.1 functions/opdracht 7.1/app.js
@@ -0,0 +1,10 @@
+//Oefening 1
+function calculate(getal1, getal2) {
+    getal1 = Number(prompt('Type een getal'));
+    getal2 = Number(prompt('Type een getal'));
+
+    let antwoord = getal1 + getal2;
+    alert(antwoord);
+}
+
+calculate()//Roept de functie op.
