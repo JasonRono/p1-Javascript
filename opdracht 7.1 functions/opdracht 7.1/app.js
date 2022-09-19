a72e6b56e4295bf0dec2c6968ad3f2fd96d8be80
Jason <9020599@student.zadkine.nl>
2022-09-19 13:08:55 +0200

    Bezig met oefening 2. Van week6.

--- opdracht 7.1 functions/opdracht 7.1/app.js
+++ opdracht 7.1 functions/opdracht 7.1/app.js
@@ -8,3 +8,12 @@ function calculate(getal1, getal2) {
 }
 
 calculate()//Roept de functie op.
+
+//Oefening 2
+function calculate(getal1, getal2) {
+    getal1 = Number(prompt('Type een getal'));
+    getal2 = Number(prompt('Type een getal'));
+
+    let antwoord = getal1 + getal2;
+    alert(antwoord);
+}
