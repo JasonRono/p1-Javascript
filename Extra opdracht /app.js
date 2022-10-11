43417086c16f4b03c1d2519f4ee43a910a635c47
Jason <9020599@student.zadkine.nl>
2022-10-11 20:53:23 +0200

    opdracht 8.1 af.

---
+++ Extra opdracht /app.js
@@ -0,0 +1,14 @@
+const closedFace = document.querySelector('.closed');
+const openFace = document.querySelector('.open');
+
+closedFace.addEventListener("click", closedFunction);
+
+function closedFunction() {
+    alert('hello');
+}
+
+openFace.addEventListener("click", openFunction);
+
+function openFunction() {
+
+}
---
+++ opdracht 8.1/app.js
@@ -0,0 +1,23 @@
+let inputValue1 = document.getElementById('input1');
+let inputValue2 = document.getElementById('input2');
+
+let answerText = document.getElementById('anwser');
+
+function multiply() {
+    answerText.innerText = 'Het antwoord is ' + inputValue1.value * inputValue2.value;;
+}
+
+function minus() {
+    let minusAnswer = inputValue1.value - inputValue2.value;
+    answerText.innerText = 'Het antwoord is ' + minusAnswer;
+}
+
+function add() {
+    let AddAnswer = inputValue1.value + inputValue2.value;
+    answerText.innerText = 'Het antwoord is ' + AddAnswer;
+}
+
+function devide() {
+    let devideAnswer = inputValue1.value / inputValue2.value;
+    answerText.innerText = 'Het antwoord is ' + devideAnswer;
+}
