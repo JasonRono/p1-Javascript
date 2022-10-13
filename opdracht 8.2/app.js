e1be012cdb2875d7154e01ee5a5f3e0a9a3fe6c3
Jason <9020599@student.zadkine.nl>
2022-10-13 11:13:09 +0200

    .

--- opdracht 8.2/app.js
+++ opdracht 8.2/app.js
@@ -1,41 +1,41 @@
-let inputValue1 = document.getElementById('input1');
+let inputValue1 = document.getElementById('input1');//Pakt de id van de input element.
 let inputValue2 = document.getElementById('input2');
 
-let answerText = document.getElementById('anwser');
+let answerText = document.getElementById('anwser');//Pakt de id van de h1 element.
 
+//Begin van de functie.
 function multiply() {
-    let multiplyAnswer = inputValue1.value * inputValue2.value;
-    if (multiplyAnswer > 0) {
-        answerText.innerText = 'Het antwoord is ' + multiplyAnswer;
+    let multiplyAnswer = inputValue1.value * inputValue2.value;//Variabele met erin de 2 waardes die in de input elementen zitten. Hierin word meteen het antwoord berekend.
+    if (inputValue1.value < 0 && inputValue2.value < 0) {//If statement met de && operator waarbij word gekeken of de 2 input waardes niet lager zijn 0.
+        answerText.innerText = 'Het input waardes zijn te laag';//Hier word het antwoord op het scherm gezet. 
     } else {
-        answerText.innerText = 'Het getal is te laag';
+        answerText.innerText = 'Het antwoord is ' + multiplyAnswer;
     }
-}
+}//Einde van de functie.
 
 function minus() {
     let minusAnswer = inputValue1.value - inputValue2.value;
-    if (minusAnswer > 0) {
-        answerText.innerText = 'Het antwoord is ' + minusAnswer;
+    if (inputValue1.value < 0 && inputValue2.value < 0) {
+        answerText.innerText = 'Het input waardes zijn te laag';
     } else {
-        answerText.innerText = 'Het getal is te laag';
+        answerText.innerText = 'Het antwoord is ' + minusAnswer;
     }
 }
 
 function add() {
     let AddAnswer = inputValue1.value + inputValue2.value;
-    if (AddAnswer > 0) {
-        answerText.innerText = 'Het antwoord is ' + AddAnswer;
+    if (inputValue1.value < 0 && inputValue2.value < 0) {
+        answerText.innerText = 'Het input waardes zijn te laag';
     } else {
-        answerText.innerText = 'Het getal is te laag';
+        answerText.innerText = 'Het antwoord is ' + AddAnswer;
     }
 }
 
 function devide() {
     let devideAnswer = inputValue1.value / inputValue2.value;
-    if (devideAnswer > 0) {
-        answerText.innerText = 'Het antwoord is ' + devideAnswer;
+    if (inputValue1.value < 0 && inputValue2.value < 0) {
+        answerText.innerText = 'Het input waardes zijn te laag';
     } else {
-        answerText.innerText = 'Het getal is te laag';
+        answerText.innerText = 'Het antwoord is ' + devideAnswer;
     }
 }
-
