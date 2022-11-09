828892f6cde5e384f65fa7d42ca4b7039f449660
Jason <9020599@student.zadkine.nl>
2022-11-09 12:01:27 +0100

    Aangepast

--- opdracht 8.1/app.js
+++ opdracht 8.1/app.js
@@ -19,4 +19,8 @@ function add() {
 function devide() {
     let devideAnswer = inputValue1.value / inputValue2.value;
     answerText.innerText = 'Het antwoord is ' + devideAnswer;
+
+    if (devideAnswer == 0) {
+        answerText.innerText = 'Het anwtoord is ';
+    }
 }
